2f3b0013e0be0636097ec2cf2bfdccb5a0255aaf
Maciel Ripetti <maciel.veliz@sansano.usm.cl>
2024-04-08 11:56:15 -0400

    clase 5

--- Sesion5/js/scripts.js
+++ Sesion5/js/scripts.js
@@ -1,5 +1,30 @@
-alert("hola mundoo")
+var cont=0
+function cambia() {
+cont++;
+switch(cont%4+1)
+ {
+ case 1:
+ document.getElementById("fotocambia").src="img/eye2.png";
+ break;
+ case 2:
+ document.getElementById("fotocambia").src="img/eye3.png";
+ break;
+ }
+
+}
+function inicio() {
+setInterval(cambia, 1000);
+}
+window.onload=inicio;
 
 function saludar(){
     alert("holiis")
+}
+
+function colorear(){
+   // console.log("si")
+   var h1 = document.getElementById("saludo")
+   h1.innerHTML = "JavaScript"
+   h1.classList.add("fondo")
+
 }
